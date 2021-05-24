76415a2cb19384a12a385e48a48c19a31ad1792a
sinda <sindajeddey@gmail.com>
2021-05-24 11:37:20 +0100

    Adding chat list, chat item and navbar

--- Eullo-Front/src/app/app-routing.module.ts
+++ Eullo-Front/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ChatListComponent} from "./chat-list/chat-list.component";
 
 const routes: Routes = [{
+  path:'',
+  component: ChatListComponent,
+  pathMatch:'full'
+},{
   path:'login',
   component:LoginComponent
 }, {
--- Eullo-Front/src/app/app.module.ts
+++ Eullo-Front/src/app/app.module.ts
@@ -11,12 +11,22 @@ import { MatInputModule } from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChatListComponent } from './chat-list/chat-list.component';
+import { ChatItemComponent } from './chat-list/chat-item/chat-item.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavbarComponent,
+    ChatListComponent,
+    ChatItemComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +36,11 @@ import { RegisterComponent } from './register/register.component';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    FontAwesomeModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
