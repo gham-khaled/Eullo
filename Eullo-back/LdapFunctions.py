68ba18300d53baf9c7ac2f517785ae3a8d7c2aa9
Khaled Ghamgui <gham.khaled@gmail.com>
2021-05-24 20:02:15 +0100

    Adding get and post user

--- Eullo-back/LdapFunctions.py
+++ Eullo-back/LdapFunctions.py
@@ -1,33 +1,49 @@
+import ldap3
 from ldap3 import Server, Connection, ALL, MODIFY_REPLACE
 import json
 from flask import request
 import hashlib
 import ast
 
+
 class LdapFunctions:
-    
+
     def __init__(self):
 
-        self.server_ip = ''
-        self.ldap_username = 'cn=admin,dc=insat,dc=chat,dc=com'
-        self.ldap_password = 'azerty123'
-        self.ldap_server = Server(self.server_ip, get_info=ALL)
-        self.conn = Connection(self.server_ip, self.ldap_username, self.ldap_password, auto_bind=True)
+        self.host = 'ec2-34-247-197-157.eu-west-1.compute.amazonaws.com'
+        self.dc = 'dc=ec2-34-247-197-157,dc=eu-west-1,dc=compute,dc=amazonaws,dc=com'
+        self.dn = f'ou=Students,{self.dc}'
+        self.ldap_username = f'cn=admin,{self.dc}'
+        self.ldap_password = 'douda123'
+        # self.attributes =  ['displayName', 'uid', 'givenName', 'userPKCS12', 'sn', 'userSMIMECertificate']
+        self.attributes = ['uid', 'givenName', 'sn']
+        # self.ldap_server = Server(self.server_ip, get_info=ALL)
+        # self.conn = Connection(self.server_ip, self.ldap_username, self.ldap_password, auto_bind=True)
+
+        self.ldap_server = Server(host=self.host, port=389, use_ssl=False, get_info='ALL')
+
+        # define the connection
+        self.conn = Connection(self.ldap_server, user=self.ldap_username, password=self.ldap_password, version=3,
+                               authentication='SIMPLE', client_strategy='SYNC', read_only=False, raise_exceptions=True)
 
     def connect(self):
-        self.conn.bind()
+        if not self.conn.bind():
+            return f'error in binding {self.conn.result} '
+        else:
+            return 'Bind is successful!!'
 
     def disconnect(self):
         self.conn.unbind()
-    
+
     def get_users(self):
         try:
             self.connect()
-            self.conn.search('ou=People,dc=insat,dc=chat,dc=com', '(&(objectclass=inetOrgPerson)(!(uid=0001)))', attributes=['*'])
+            self.conn.search('ou=Students,dc=insat,dc=chat,dc=com', '(&(objectclass=inetOrgPerson)(!(uid=0001)))',
+                             attributes=['*'])
             users = []
             for entry in self.conn.entries:
                 user = json.loads(entry.entry_to_json())
-                if('userSMIMECertificate' in user['attributes']):
+                if ('userSMIMECertificate' in user['attributes']):
                     L = ast.literal_eval(user['attributes']['userSMIMECertificate'][0])
                     user['attributes']['userCertificate'] = str(np.array(L, dtype='int8').tobytes())
                 users.append(user)
@@ -39,38 +55,38 @@ class LdapFunctions:
             return 'authentication error'
 
     def get_user(self, username=None):
-        if(username == None):
+        if username == None:
             return {}
-        try:
-            self.connect()
-            #attributes=['cn', 'sn']
-            self.conn.search('ou=People,dc=insat,dc=chat,dc=com', '(&(objectclass=inetOrgPerson)(sn=' + username + ')(!(uid=0001)))', attributes=['displayName', 'uid', 'givenName', 'userPKCS12', 'sn', 'userSMIMECertificate'])
-            if(not self.conn.entries):
-                return "User doesn't exist"
-            else:
-                user = json.loads(self.conn.entries[0].entry_to_json())
-                if('userSMIMECertificate' in user['attributes']):
-                    L = ast.literal_eval(user['attributes']['userSMIMECertificate'][0])
-                    user['attributes']['userCertificate'] = str(np.array(L, dtype='int8').tobytes())
+        print(username)
+        self.connect()
+        # attributes=['cn', 'sn']
+        self.conn.search(self.dn, f'(&(objectclass=inetOrgPerson)(sn={username})(!(uid=0001)))',
+                         attributes=self.attributes)
+        if not self.conn.entries:
+            return "User doesn't exist"
+        else:
+            user = json.loads(self.conn.entries[0].entry_to_json())
+            # if ('userSMIMECertificate' in user['attributes']):
+            #     L = ast.literal_eval(user['attributes']['userSMIMECertificate'][0])
+            #     # user['attributes']['userCertificate'] = str(np.array(L, dtype='int8').tobytes())
 
             self.disconnect()
             return user
-        except ldap3.LDAPError:
-            self.conn.unbind()
-            return 'authentication error'
 
     def login(self, username, password):
         try:
             self.connect()
             m = hashlib.sha256(str(password).encode('utf-8'))
             hashed_pass = m.hexdigest()
-            self.conn.search('ou=People,dc=insat,dc=chat,dc=com', '(&(objectclass=inetOrgPerson)(sn=' + username + ')(userPassword=' + hashed_pass + '))', attributes=['sn', 'displayName', 'givenName', 'uid', 'userPKCS12', 'userSMIMECertificate'])
-            if(not self.conn.entries):
+            self.conn.search('ou=Students,dc=insat,dc=chat,dc=com',
+                             '(&(objectclass=inetOrgPerson)(sn=' + username + ')(userPassword=' + hashed_pass + '))',
+                             attributes=['sn', 'displayName', 'givenName', 'uid', 'userPKCS12', 'userSMIMECertificate'])
+            if (not self.conn.entries):
                 return None
             else:
                 user = json.loads(self.conn.entries[0].entry_to_json())
                 print(user)
-                if('userSMIMECertificate' in user['attributes']):
+                if ('userSMIMECertificate' in user['attributes']):
                     L = ast.literal_eval(user['attributes']['userSMIMECertificate'][0])
                     user['attributes']['userCertificate'] = str(np.array(L, dtype='int8').tobytes())
 
@@ -80,53 +96,47 @@ class LdapFunctions:
             self.conn.unbind()
             return 'authentication error'
 
-    def add_user(self):
-        try:
-            self.connect()
-            user = json.loads(request.data)
-            #print(user)
+    def add_user(self, user):
+        self.connect()
+        print(user)
+        # before adding i need to select by username to make sure it's unique
+        self.conn.search(f'ou=Students,{self.dc}', f"(&(objectclass=inetOrgPerson)(sn={user['username']}))",
+                         attributes=['sn'])
+        if self.conn.entries:
+            self.disconnect()
+            return "User already exists"
+        else:
+            m = hashlib.sha256(str(user['userPassword']).encode('utf-8'))
+            hashed_pass = m.hexdigest()
+            result = self.conn.add(
+                f"cn={user['username']},ou=Students,{self.dc}",
+                attributes={
+                    "objectClass": "inetOrgPerson",
+                    "sn": user['username'],
+                    "givenName": user['name'],
+                    "uid": user['number'],
 
-            #before adding i need to select by username to make sure it's unique
-            username = user['sn']
-            self.conn.search('ou=People,dc=insat,dc=chat,dc=com', '(&(objectclass=inetOrgPerson)(sn=' + username + '))', attributes=['sn'])
-            if(self.conn.entries):
-                self.conn.unbind()
-                return "User already exists"
-            else:
-                m = hashlib.sha256(str(user['userPassword']).encode('utf-8'))
-                hashed_pass = m.hexdigest()
-                res = self.conn.add(
-                    'cn=' + user['sn'] + ',ou=People,dc=insat,dc=chat,dc=com',
-                    attributes={
-                        "objectClass": "inetOrgPerson",
-                        "sn":  user['sn'],
-                        "uid": user['uid'],
-                        "givenName": user['givenName'],
-                        "displayName": user['displayName'],
-                        "userPassword": hashed_pass,
-                        "userPKCS12": user['userPKCS12'],
-                        #"userCertificate": 
-                    },
-                )
+                    # "displayName": user['displayName'],
+                    "userPassword": hashed_pass,
+                    # "userPKCS12": user['userPKCS12'],
+                    # "userCertificate":
+                },
+            )
 
-                self.conn.unbind()
-                if(res):
-                    return 'User added successfully'
-                else:
-                    return 'User already exists'
-
-        except ldap3.LDAPError:
             self.conn.unbind()
-            return 'Authentication error'
+            if result:
+                return 'User added successfully'
+            else:
+                return 'Error while creating the user'
 
     def modify_user(self, username):
         try:
             self.connect()
             user = json.loads(request.data)
-            #print(user)
+            # print(user)
             m = hashlib.sha256(str(user['userPassword']).encode('utf-8'))
             hashed_pass = m.hexdigest()
-            
+
             edits = {}
             if 'displayName' in user:
                 edits['displayName'] = [(MODIFY_REPLACE, user['displayName'])]
@@ -140,12 +150,12 @@ class LdapFunctions:
                 edits['userPassword'] = [(MODIFY_REPLACE, hashed_pass)]
 
             res = self.conn.modify(
-                dn='cn=' + username + ',ou=People,dc=insat,dc=chat,dc=com',
+                dn='cn=' + username + ',ou=Students,dc=insat,dc=chat,dc=com',
                 changes=edits,
             )
 
             self.conn.unbind()
-            if(res):
+            if (res):
                 return 'User edited succefully'
             else:
                 return 'An error has occured'
@@ -155,13 +165,13 @@ class LdapFunctions:
             return 'Authentication error'
 
     def delete_user(self, username=None):
-        if(username == None):
+        if (username == None):
             return 'An error has occured'
         try:
             self.connect()
-            res = self.conn.delete('cn=' + username + ',ou=People,dc=insat,dc=chat,dc=com')
+            res = self.conn.delete('cn=' + username + ',ou=Students,dc=insat,dc=chat,dc=com')
             self.conn.unbind()
-            if(res):
+            if (res):
                 return 'User deleted succefully'
             else:
                 return 'An error has occured'
--- Eullo-back/main.py
+++ Eullo-back/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import flask.scaffold
+
+flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Api, Resource
 from flask_cors import CORS
+from LdapFunctions import LdapFunctions
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+ldapFunctions = LdapFunctions()
 
 
 class User(Resource):
@@ -16,8 +21,21 @@ class User(Resource):
 
     def delete(self, username):
         return ldapFunctions.delete_user(username)
+
+    def post(self):
+        user = request.get_json()
+        return ldapFunctions.add_user(user)
     ######### LDAP
 
+
+class Auth(Resource):
+    def post(self):
+        user = request.get_json()
+        return ldapFunctions.add_user(user)
+
+
+api.add_resource(User, '/user/<username>')
+api.add_resource(Auth, '/auth')
 
 if __name__ == '__main__':
     app.run(debug=True)
